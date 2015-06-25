427723f8d33386dcfa76f184c62ce57f74b2b8df
ccshark <gustav.larss@gmail.com>
2015-06-25 19:39:00 +0200

    uplaasd

---
+++ js/controllers.js
@@ -0,0 +1,33 @@
+angular.module('starter.controllers', [])
+
+    .controller('MapCtrl', function($scope, $ionicLoading, $timeout) {
+
+})
+	
+	
+    .controller('cameraCtrl', function($ionicPlatform, $rootScope, $scope, $cordovaCamera) {
+        $ionicPlatform.ready(function() {
+
+
+            var options = {
+                quality: 50,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                targetWidth: 100,
+                targetHeight: 100,
+                popoverOptions: CameraPopoverOptions,
+                saveToPhotoAlbum: false
+            };
+
+            $scope.takePicture = function() {
+                $cordovaCamera.getPicture(options).then(function(imageData) {
+                    $scope.imgSrc = "data:image/jpeg;base64," + imageData;
+                }, function(err) {
+                    console.log(err);
+                });
+            }
+ 
+    });
+});
